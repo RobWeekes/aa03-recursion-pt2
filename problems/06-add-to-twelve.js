923aa7859c08a820d9847f71fa16e4ef2c355698
robweekes <weekes.robert@gmail.com>
2024-04-12 18:40:47 -0700

    problem 6 both ways

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -14,44 +14,65 @@ addToTwelve([1]); // false
 
 
 function addToTwelve(arr) {
-  // Your code here
-  if(arr.length < 2 ){
-    return false;
+
+  if(arr.length === 0){
+      return false;
   }
-    if(arr.length > 2){
-    let last = arr[arr.length - 1]
-    console.log(last)
-    if (last + arr[arr.length - 2] === 12){
+  if(arr.length !== 0){
+      let last = arr[arr.length - 1]
+      console.log(last)
+  if (last + arr[arr.length - 2] === 12){
       return true;
-    } else{
-     arr = arr.pop(last)
-    console.log(arr)
-    return addToTwelve(arr);
-    }
+  } else {
+      arr.pop(last);
+      console.log(arr);
+      return addToTwelve(arr);
+  }
   }
 }
 
-console.log
-
-
-//   if(arr[0] + arr[1] === 12){
-//     return true;
-//   }
-//   else {
-//     return addToTwelve(arr.slice(1));
-//   }
-
-// }
-
-
-
-
-
-// console.log(addToTwelve([1, 3, 4, 7, 5])); // true
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
 console.log(addToTwelve([1, 3, 4, 7, 6])); // false
-// console.log(addToTwelve([1, 11, 4, 7, 6])); // true
-// console.log(addToTwelve([1, 12, 4, 7, 6])); // false
-// console.log(addToTwelve([1])); // false
+console.log(addToTwelve([1, 11, 4, 7, 6])); // true
+console.log(addToTwelve([1, 12, 4, 7, 6])); // false
+console.log(addToTwelve([1])); // false
+
+
+  // Davidson
+
+// let newArr = [];
+function addToTwelve2(arr) {
+  // debugger
+  // console.log(arr);
+  // arr.slice(1);       // slice doesnt change the original arr
+  // console.log(arr);
+  // newArr = arr.slice(1);
+  // console.log(newArr);   // dont need newArr, passing in sliced array on line 63
+
+  if(arr.length === 0 ){
+      return false;
+  }
+
+  if(arr.length !== 0){
+      let last = arr[arr.length - 1];
+      console.log(last);
+      if(last + arr[arr.length - 2] === 12){
+          return true;
+      }
+      else {
+          // return addToTwelve2(arr.slice(1));
+          return addToTwelve2(arr.slice(0, arr.length - 1));
+      }
+  }
+}
+
+console.log(addToTwelve2([1, 3, 4, 7, 5])); // true
+console.log(addToTwelve2([1, 3, 4, 7, 6])); // false
+console.log(addToTwelve2([1, 11, 4, 7, 6])); // true
+console.log(addToTwelve2([1, 12, 4, 7, 6])); // false
+console.log(addToTwelve2([1])); // false
+
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
